5ec45f534998fe2d4a9993751c6f848994a98298
c7w <cc7w@foxmail.com>
2025-07-27 17:05:02 +0800

    init project

---
+++ src/.config/user.ts
@@ -0,0 +1,11 @@
+import type { UserConfig } from '~/types'
+
+export const userConfig: Partial<UserConfig> = {
+  // Override the default config here
+  site: { 
+    website: "https://new-blog.c7w.tech",
+    title: "C7W's Blog",
+    author: "c7w",
+    description: "C7W's personal blog"
+  },
+}
